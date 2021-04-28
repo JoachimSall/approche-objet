9c7ad452d05a53908d0bf3a3121e5d84cf3dd45d
Joachim <joachimsall8@gmail.com>
2021-04-28 12:02:02 +0200

    utilisation accesseurs

--- src/fr/diginamic/banque/entites/TestBanque.java
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -5,7 +5,7 @@ public class TestBanque {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Compte compte1 = new Compte("23444", 200);
-		System.out.println(compte1);
+		System.out.println(compte1.getNumero() + " " + compte1.getSolde());
 
 	}
 
