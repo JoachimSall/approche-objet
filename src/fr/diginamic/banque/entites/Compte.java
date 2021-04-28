452a012eb02514d9757c4a7febc27f21e2dabc21
Joachim <joachimsall8@gmail.com>
2021-04-28 11:47:42 +0200

    creation classe compte avec attribut private et accesseurs

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,29 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	private String numero;
+	private int solde;
+	
+	public Compte(String numero, int solde) {
+		this.numero = numero;
+		this.solde = solde;
+	}
+	
+	public String getNumero() {
+		return numero;
+		
+	}
+	
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+	public int getSolde() {
+		return solde;
+		
+	}
+	
+	public void setSolde(int solde) {
+		this.solde = solde;
+	}
+}
